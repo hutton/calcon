e9da6a0656d4c42ca65d8d7cfbcc724362b7bda3
Simon <simon.hutton@gmail.com>
2014-09-03 20:41:08 +0100

    Added downloading.py

---
+++ downloading.py
@@ -0,0 +1,252 @@
+import StringIO
+import logging
+import os
+import sys
+import time
+from google.appengine.api import memcache
+from xhtml2pdf import pisa
+from helper import process_calendar, log_download, support_email
+
+sys.path.insert(0, 'libs')
+
+import re
+import webapp2
+import conversion
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import template
+import tablib
+import icalendar
+from google.appengine._internal.django.utils import simplejson
+
+__author__ = 'simonhutton'
+
+column_order = ['Summary', 'Description', 'Start', 'End', 'Organizer', 'Attendees', 'Location', 'Created']
+
+
+def order_columns(column_name, keys):
+    return column_name in keys
+
+
+def build_tab_lib_dataset(events):
+    data = tablib.Dataset()
+
+    keys = list(set([key for event in events for key in event.keys()]))
+
+    ordered_columns = [item for item in column_order if order_columns(item, keys)]
+
+    rows = [[event.get(key, '') for key in ordered_columns] for event in events]
+
+    data.headers = ordered_columns
+
+    for row in rows:
+        data.append(row)
+
+    return data
+
+
+def generate_csv_content(events):
+    data = build_tab_lib_dataset(events)
+
+    return data.csv
+
+
+def generate_xls_content(events):
+    data = build_tab_lib_dataset(events)
+
+    return data.xls
+
+
+def generate_xlsx_content(events):
+    data = build_tab_lib_dataset(events)
+
+    return data.xlsx
+
+
+def generate_json_content(events):
+    data = build_tab_lib_dataset(events)
+
+    return data.json
+
+
+def generate_tsv_content(events):
+    data = build_tab_lib_dataset(events)
+
+    return data.tsv
+
+
+def generate_txt_content(events):
+    path = os.path.join(os.path.join(os.path.dirname(__file__), 'html'), '../templates/txt_template.txt')
+
+    return template.render(path, {'events': events})
+
+
+def generate_xml_content(events):
+    path = os.path.join(os.path.join(os.path.dirname(__file__), 'html'), '../templates/xml_template.xml')
+
+    return template.render(path, {'events': events})
+
+
+def generate_html_content(events, filename):
+    path = os.path.join(os.path.join(os.path.dirname(__file__), 'html'), '../templates/html_template.html')
+
+    return template.render(path, {'events': events, 'filename': filename})
+
+
+def generate_pdf_content(events, filename):
+    path = os.path.join(os.path.join(os.path.dirname(__file__), 'html'), '../templates/pdf_template.html')
+
+    html_output = template.render(path, {'events': events, 'filename': filename})
+
+    pdf_output = StringIO.StringIO()
+
+    # convert HTML to PDF
+    pisa_status = pisa.CreatePDF(html_output, dest=pdf_output)
+
+    if pisa_status.err:
+        logging.error('Failed to covert to pdf')
+        raise
+
+    pdf_output.seek(0)
+
+    return pdf_output.getvalue()
+
+
+class Downloading(webapp2.RequestHandler):
+    @staticmethod
+    def get_conversion_from_hash(file_hash):
+        query = conversion.Conversion.gql("WHERE hash = :hash", hash=file_hash)
+        conversions = query.fetch(1)
+        if conversions:
+            return conversions[0]
+        else:
+            return None
+
+    def get(self):
+
+        # noinspection PyBroadException
+        try:
+            matches = re.match(
+                r"/download/(?P<hash>[0-9a-z]+)/(?P<filename>[-\w^&'@{}[\],$=!#().%+~ ]+).(?P<extension>txt|csv|xlsx|xls|json|tsv|xml|html|pdf)",
+                self.request.path)
+
+            if matches:
+                file_hash = matches.group("hash")
+                filename = matches.group("filename")
+                extension = matches.group("extension")
+
+                download_id = file_hash + '_' + filename + '.' + extension
+
+                self.response.set_cookie(download_id)
+
+                start_time = time.time()
+
+                current_conversion = self.get_conversion_from_hash(file_hash)
+
+                if file_hash == 'sample':
+                    path = os.path.join(os.path.join(os.path.dirname(__file__), 'html'), '../templates/error.html')
+                    self.response.out.write(template.render(path, {'status': '???',
+                                                                   'message': "Upload a valid .ics file and we'll<br/>convert it to .csv, .xlsx, .html or .pdf."}))
+
+                    self.response.status = 200
+                else:
+                    if current_conversion:
+                        blob_reader = blobstore.BlobReader(current_conversion.blob_key)
+
+                        file_content = blob_reader.read()
+
+                        gcal = icalendar.Calendar.from_ical(file_content)
+
+                        events = process_calendar(gcal)
+
+                        self.response.headers['Content-Transfer-Encoding'] = 'binary'
+                        self.response.headers['Accept-Range'] = 'bytes'
+                        self.response.headers['Content-Length'] = str(current_conversion.file_size)
+                        self.response.headers['Content-Encoding'] = 'binary'
+                        self.response.headers['Content-Disposition'] = 'attachment; filename=' + filename + '.' + extension
+
+                        if extension == 'csv':
+                            self.response.headers['Content-Type'] = 'application/csv'
+                            output_content = generate_csv_content(events)
+
+                            log_download(current_conversion, time.time() - start_time, extension)
+
+                            self.response.out.write(output_content)
+                        else:
+                            if current_conversion.paid_date:
+                                if extension == 'xls':
+                                    self.response.headers['Content-Type'] = 'application/xls'
+                                    output_content = generate_xls_content(events)
+
+                                if extension == 'xlsx':
+                                    self.response.headers['Content-Type'] = 'application/xlsx'
+                                    output_content = generate_xlsx_content(events)
+
+                                if extension == 'json':
+                                    self.response.headers['Content-Type'] = 'application/json'
+                                    output_content = generate_json_content(events)
+
+                                if extension == 'tsv':
+                                    self.response.headers['Content-Type'] = 'application/tsv'
+                                    output_content = generate_tsv_content(events)
+
+                                if extension == 'txt':
+                                    self.response.headers['Content-Type'] = 'application/txt'
+                                    output_content = generate_txt_content(events)
+
+                                if extension == 'xml':
+                                    self.response.headers['Content-Type'] = 'application/xml'
+                                    output_content = generate_xml_content(events)
+
+                                if extension == 'html':
+                                    self.response.headers['Content-Type'] = 'application/html'
+                                    output_content = generate_html_content(events, filename + '.' + extension)
+
+                                if extension == 'pdf':
+                                    self.response.headers['Content-Type'] = 'application/pdf'
+                                    output_content = generate_pdf_content(events, filename + '.' + extension)
+
+                                log_download(current_conversion, time.time() - start_time, extension)
+
+                                self.response.out.write(output_content)
+                            else:
+                                support_email('Download Failed',
+                                              "Trying to download file type (" + extension + ") which hasn't been paid for, hash: " + file_hash)
+
+                                logging.warn(
+                                    "Trying to download file type (" + extension + ") which hasn't been paid for, hash: " + file_hash)
+
+                                path = os.path.join(os.path.join(os.path.dirname(__file__), 'html'), '../templates/error.html')
+                                self.response.out.write(template.render(path, {'status': '404',
+                                                                               'message': "We don't have what you're looking for."}))
+
+                                self.response.status = 404
+                    else:
+                        support_email('Download Failed', 'Could not find hash: ' + file_hash)
+
+                        logging.warn('Could not find hash: ' + file_hash)
+
+                        path = os.path.join(os.path.join(os.path.dirname(__file__), 'html'), '../templates/error.html')
+                        self.response.out.write(template.render(path, {'status': '404',
+                                                                       'message': "We don't have what you're looking for."}))
+
+                        self.response.status = 404
+            else:
+                support_email('Download Failed', 'Could not parse path: ' + self.request.path)
+
+                logging.warn('Could not parse path: ' + self.request.path)
+
+                path = os.path.join(os.path.join(os.path.dirname(__file__), 'html'), '../templates/error.html')
+                self.response.out.write(template.render(path, {'status': '404',
+                                                               'message': "We don't have what you're looking for."}))
+
+                self.response.status = 404
+        except Exception, e:
+            support_email('Download Failed', 'Exception: ' + e.message)
+
+            logging.error(e.message)
+
+            path = os.path.join(os.path.join(os.path.dirname(__file__), 'html'), '../templates/error.html')
+            self.response.out.write(template.render(path, {'status': '500',
+                                                           'message': "Something bad happened, we're looking at it."}))
+
+            self.response.status = 500
