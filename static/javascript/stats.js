0bb2dceaa712cfad16dd5c1e8f7bb4027035d953
Simon <simon.hutton@gmail.com>
2014-09-08 18:14:23 +0100

    Added trend to stats

--- static/javascript/stats.js
+++ static/javascript/stats.js
@@ -39,6 +39,49 @@ window.StatsView = Backbone.View.extend({
     className: 'stat-section'
 });
 
+window.TrendView = Backbone.View.extend({
+    initialize: function () {
+        this.render();
+    },
+
+    template: _.template($('#trend-view-template').html()),
+
+    render: function(){
+        this.$el.html(this.template(this.model));
+
+        this.chartEl = this.$el.find('.download-type-chart');
+
+        var ctx = this.chartEl.get(0).getContext("2d");
+
+        var data = {
+            labels: this.model.labels,
+            datasets: [
+            {
+                label: "Uploads",
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(220,220,220,0.75)",
+                highlightStroke: "rgba(220,220,220,1)",
+                data: this.model.uploads
+            },
+            {
+                label: "Downloads",
+                fillColor: "rgba(151,187,205,0.5)",
+                strokeColor: "rgba(151,187,205,0.8)",
+                highlightFill: "rgba(151,187,205,0.75)",
+                highlightStroke: "rgba(151,187,205,1)",
+                data: this.model.downloads
+            }
+        ]};
+
+        new Chart(ctx).Bar(data);
+
+        return this;
+    },
+
+    className: 'stat-section'
+});
+
 
 window.App = Backbone.View.extend({
     initialize: function (){
@@ -59,7 +102,11 @@ window.App = Backbone.View.extend({
 
             that.statsListEl.empty();
 
-            _.each(response, function(statDataItem){
+            var trendView = new TrendView({model: response.trend})
+
+            that.statsListEl.append(trendView.el);
+
+            _.each(response.days, function(statDataItem){
                 var view = new StatsView({model: statDataItem});
 
                 that.statsListEl.append(view.el);
--- statistics.py
+++ statistics.py
@@ -21,6 +21,33 @@ def extract_data(items):
             'downloads': len(downloads),
             'fileTypeFrequencies': file_type_frequencies}
 
+def same_day(date1, date2):
+    return date1.year == date2.year and date1.month == date2.month and date1.day == date2.day
+
+def extract_trend_data(items):
+
+    items.sort(key=lambda download_item: download_item.created_date)
+
+    first_date = items[0].created_date
+    last_date = items[len(items) - 1].created_date
+
+    current_date = first_date
+
+    uploads = []
+    downloads = []
+    labels = []
+
+    while current_date < last_date:
+        matches = [item for item in items if same_day(current_date, item.created_date)]
+
+        uploads.append(len([match for match in matches if not match.download]))
+        downloads.append(len([match for match in matches if match.download]))
+        labels.append(current_date.strftime('%a %d %b'))
+
+        current_date = current_date + timedelta(1)
+
+    return labels, uploads, downloads
+
 
 class Statistics(webapp2.RequestHandler):
     def get(self):
@@ -51,9 +78,18 @@ class StatisticsData(webapp2.RequestHandler):
         all_values = extract_data(all_downloads)
         all_values['title'] = "All"
 
-        response = [today_values,
+        trend_labels, trend_uploads, trend_downloads = extract_trend_data(month_items)
+
+        trend_data = {'labels': trend_labels,
+                      'uploads': trend_uploads,
+                      'downloads': trend_downloads}
+
+        days_data = [today_values,
                     month_values,
                     all_values]
+
+        response = {'days': days_data,
+                    'trend': trend_data}
 
         self.response.out.write(simplejson.dumps(response))
 
